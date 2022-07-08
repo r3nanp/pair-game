2f08f90b12dbd8b0f3fb060a98677d326351741e
Renan Pereira <renan.souza.dev@gmail.com>
2022-07-08 20:25:49 -0300

    feat: extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#63b3ed'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
