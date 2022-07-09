354f9ed5206f425382fcdde976b602034d4a7bf3
Renan Pereira <renan.souza.dev@gmail.com>
2022-07-08 22:03:01 -0300

    feat(game): add a way to change the pairs

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,26 +1,51 @@
-type Pair = {
-  lang: string
-  value: string
-}
+import { useEffect, useState } from 'react'
 
 type Lang = {
   code: string
   name: string
 }
 
+type Choice = {
+  lang: string
+  value: string
+}
+
 type GameProps = {
-  pairs: Pair[]
+  pairs: string[][]
   firstLang: Lang
   secondLang: Lang
 }
 
 export const Game = ({ pairs = [], firstLang, secondLang }: GameProps) => {
+  const [choices, setChoices] = useState<Choice[]>([])
+
+  useEffect(() => {
+    const allPairs = pairs.flatMap(([pairA, pairB]) => [
+      {
+        lang: firstLang.code,
+        value: pairA
+      },
+      {
+        lang: secondLang.code,
+        value: pairB
+      }
+    ])
+
+    const sortedPairs = allPairs.sort(() => Math.random() * 0.5)
+
+    setChoices(sortedPairs)
+  }, [firstLang.code, pairs, secondLang.code])
+
   return (
     <>
       <h2 className="mx-4">Choose your accent</h2>
-      <div className="flex"></div>
+      <div className="flex">
+        <ul className="flex flex-wrap p-2"></ul>
+        <ul className="flex flex-wrap p-2"></ul>
+      </div>
 
       <h2 className="mx-4">Choose the pairs</h2>
+      <ul className="mr-2 flex flex-wrap"></ul>
     </>
   )
 }
