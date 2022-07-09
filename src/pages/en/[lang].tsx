417fa798c967b44a6277207013ff512e9372cea1
Renan Pereira <renan.souza.dev@gmail.com>
2022-07-08 22:05:21 -0300

    feat(en): create initial dynamic page

--- src/pages/en/[lang].tsx
+++ src/pages/en/[lang].tsx
@@ -1,22 +1,28 @@
 import { Game, Header, Layout } from 'components'
+import { Content, Langs } from 'data/langsData'
+import { useResource } from 'lib/useResource'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+const fetcher = async (lang: Langs) =>
+  await (await fetch(`/api/pairs?lang=${lang}`)).json()
 
 const Lang: NextPage = () => {
+  const { query } = useRouter()
+  const lang = query.lang as Langs
+
+  const { data, isError, isLoading } = useResource<Content>(lang, () =>
+    fetcher(lang)
+  )
+
+  if (!data || isLoading) return <p>Loading...</p>
+  if (isError) return <p>Something went wrong</p>
+
   return (
     <Layout>
       <Header />
 
-      <Game
-        pairs={[]}
-        firstLang={{
-          code: 'en',
-          name: 'Spanish'
-        }}
-        secondLang={{
-          code: 'en',
-          name: 'Spanish'
-        }}
-      />
+      <Game pairs={data.pairs} firstLang={data.langA} secondLang={data.langB} />
     </Layout>
   )
 }
