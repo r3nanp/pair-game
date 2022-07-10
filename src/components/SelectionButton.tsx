1f40370728aedc8fd2d7b113c40ba7cd787325b2
Renan Pereira <renan.souza.dev@gmail.com>
2022-07-10 20:01:11 -0300

    fix(selection button): add `rest` and change UI

--- src/components/SelectionButton.tsx
+++ src/components/SelectionButton.tsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx'
+import { ButtonHTMLAttributes } from 'react'
 
 const variants = {
-  primary: 'border-gray-300 text-black bg-main/20',
-  reset: 'border-red-200 text-black bg-red-200/20',
-  selected: 'border-green-200 text-black bg-green-200/20'
+  primary: 'border-gray-300 text-white bg-transparent',
+  reset: 'border-red-200 text-white bg-red-200/20',
+  selected: 'border-green-200 text-white bg-green-200/20'
 }
 
-export type SelectionButtonProps = {
+export type SelectionButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string
   className?: string
   variant?: keyof typeof variants
@@ -15,12 +16,14 @@ export type SelectionButtonProps = {
 export const SelectionButton = ({
   label,
   variant = 'primary',
-  className = ''
+  className = '',
+  ...rest
 }: SelectionButtonProps) => {
   return (
     <button
+      {...rest}
       className={clsx(
-        'rounded-full border-2 py-2 px-4 ',
+        'rounded-full border-2 py-2 px-4 disabled:cursor-not-allowed disabled:opacity-70',
         variants[variant],
         className
       )}
